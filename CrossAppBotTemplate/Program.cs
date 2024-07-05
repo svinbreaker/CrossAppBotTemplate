149ab510e83d3e56b50bc564cf552499566b7de0
svinbreaker <veldyaev2013@mail.ru>
2024-07-05 18:35:51 +0400

    Fixed non-working commands

--- CrossAppBotTemplate/Program.cs
+++ CrossAppBotTemplate/Program.cs
@@ -42,10 +42,10 @@ namespace CrossAppBotTemplate
 
             //Comment out the lines for creating those bots that are not needed
             VkBotClient vkBot = new VkBotClient(Convert.ToString(botsData.vk.token), ulong.Parse(Convert.ToString(botsData.vk.groupId)));
-            TelegramBotClient telegramBot = new TelegramBotClient(Convert.ToString(botsData.telegram.token), DataPath + "/Telegram/telegramUsers.json");
+            TelegramBotClient telegramBot = new TelegramBotClient(Convert.ToString(botsData.telegram.token), DataPath + "/telegramUsers.json");
             DiscordBotClient discordBot = new DiscordBotClient(Convert.ToString(botsData.discord.token));
 
-            bots = new List<AbstractBotClient> { /*discordBot, vkBot, telegramBot */};
+            bots = new List<AbstractBotClient> { discordBot, vkBot, telegramBot };
 
             //Comment out the lines for adding those commands that are not needed
             List<AbstractCommand> universalCommands = new List<AbstractCommand>
@@ -66,9 +66,9 @@ namespace CrossAppBotTemplate
             //Comment out the lines for adding those event listeners that are not needed
             foreach (AbstractBotClient bot in bots)
             {
-                bot.EventManager.Subscribe<BotConnectedEvent>(OnConnect, RegisterSlashCommands);
-                bot.EventManager.Subscribe<BotDisconnectedEvent>(OnDisconnect);
-                bot.EventManager.Subscribe<MessageReceivedEvent>(OnMessageReceived);
+                bot.EventManager.Subscribe<BotConnectedEvent>(LogConnect, RegisterSlashCommands);
+                bot.EventManager.Subscribe<BotDisconnectedEvent>(LogDisconnect);
+                bot.EventManager.Subscribe<MessageReceivedEvent>(OnMessageReceived, CheckCommands);
                 bot.EventManager.Subscribe<MessageEditedEvent>(OnMessageEdited);
                 //etc.
 
@@ -86,12 +86,12 @@ namespace CrossAppBotTemplate
             await Task.WhenAll(telegramBot.StartAsync(), discordBot.StartAsync(), vkBot.StartAsync());
         }
 
-        private async Task OnConnect(BotConnectedEvent clientEvent)
+        private async Task LogConnect(BotConnectedEvent clientEvent)
         {
             Console.WriteLine("Connected to " + clientEvent.Client.Name);
         }
 
-        private async Task OnDisconnect(BotDisconnectedEvent clientEvent)
+        private async Task LogDisconnect(BotDisconnectedEvent clientEvent)
         {
             Console.WriteLine("Disconnected from " + clientEvent.Client.Name);
         }
@@ -104,6 +104,30 @@ namespace CrossAppBotTemplate
         public async Task OnMessageEdited(MessageEditedEvent clientEvent)
         {
 
+        }
+
+        public async Task CheckCommands(MessageReceivedEvent clientEvent) 
+        {
+            AbstractBotClient client = clientEvent.Message.Client;
+            ChatMessage message = clientEvent.Message;
+            string text = message.Text;
+            TextCommandHelper textCommandHelper = client.CommandManager.TextCommandHelper;
+
+            if (textCommandHelper != null && !string.IsNullOrEmpty(text)) 
+            {
+                if (textCommandHelper.StringIsTextCommand(text)) 
+                {
+                    AbstractCommand command = textCommandHelper.CreateCommandInstance(text, CommandContext.FromMessage(message));
+                    try
+                    {
+                        await command.Execute();
+                    }
+                    catch (Exception e) 
+                    {
+                        Console.WriteLine("Error occured while executing command " + command.Name + ": " + e.Message + "\n" + e.ToString());
+                    }
+                }
+            }
         }
 
         //etc.
